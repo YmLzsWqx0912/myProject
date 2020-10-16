bf91b95feef0ce6e9adc09845baed61012db480b
wqxYYY <1679861213@qq.com>
2020-10-16 21:22:12 +0800

    已经布局了 login register admin

---
+++ controller/index.js
@@ -0,0 +1,20 @@
+const { render } = require("../app")
+
+var login = (req, res, next) => {
+    res.render('login');
+}
+var register = (req, res, next) => {
+    res.render('register');
+}
+var admin = (req, res, next) => {
+    res.render('admin');
+}
+
+module.exports = {
+    login ,
+    register,
+    admin
+}
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+//2.引入控制层********************************MVC中的 controller层
+var controllerIndex = require('../controller/index') ;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//1.访问三个页面*****************************MVC中的  view层
+router.get('/login' ,controllerIndex.login)
 //访问login 页面
-router.get('/login' , (req, res, next) => {
-  res.render('login');
-})
+router.get('/register' , controllerIndex.register)
 //访问login 页面
-router.get('/register' , (req, res, next) => {
-  res.render('register');
-  
-})
-//访问login 页面
-router.get('/admin' , (req, res, next) => {
-  res.render('admin');
-  
-})
+router.get('/admin' , controllerIndex.admin)
 
 module.exports = router;
